089867e06ffd04dfc249e1dd939a38278a4c901b
cesar chavez <cesar.a.chavez.h@gmail.com>
2021-12-15 12:13:30 -0600

    prueba de funcionamiento

--- enfriar.py
+++ enfriar.py
@@ -2,3 +2,20 @@
 import pyautogui as pt
 import os
 import time
+
+timpo = 1
+
+busqueda = {
+    "play":"controll/play.png"
+}
+
+def mover(move,click = False):
+    time.sleep(2)
+    position = pt.locateCenterOnScreen(move)
+    print(position)
+    pt.moveTo(position) 
+    if click == True:
+        pt.click(button="left")
+    pass
+
+mover(busqueda["play"])
