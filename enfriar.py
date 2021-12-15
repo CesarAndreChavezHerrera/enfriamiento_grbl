7fcaca6e53ff99e3ca517a449687448542c8b99b
cesar chavez <cesar.a.chavez.h@gmail.com>
2021-12-15 12:19:16 -0600

    creacion de la lista de las imagenes

--- enfriar.py
+++ enfriar.py
@@ -6,7 +6,14 @@ import time
 timpo = 1
 
 busqueda = {
-    "play":"controll/play.png"
+    "play":"controll/play.png",
+    "reanudar":"controll/reanudar.png",
+    "pausa":"controll/pausa.png",
+    "pausa_causa":"controll/pausa_causa.png",
+    "opciones_inicio":"controll/opciones_inicio.png",
+    "opciones_cancelar":"controll/opciones_cancelar.png",
+    "detener":"controll/detener.png",
+    "aceptar":"controll/aceptar.png"
 }
 
 def mover(move,click = False):
